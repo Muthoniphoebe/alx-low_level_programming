274dc8530d19729035b6411c0e7e7e354485a403
Muthoniphoebe <99501695+Muthoniphoebe@users.noreply.github.com>
2022-09-30 16:07:53 +0300

    updated code

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -9,15 +9,14 @@
  
 size_t print_dlistint(const dlistint_t *h)
 {
-	int i = 0;
+	size_t nodes = 0;
 
-	if (h == NULL)
-		return (0);
-	while (h != NULL)
+	while (h)
 	{
+		nodes++;
 		printf("%d\n", h->n);
 		h = h->next;
-		i++;
 	}
-	return (i);
+
+	return (nodes);
 }
