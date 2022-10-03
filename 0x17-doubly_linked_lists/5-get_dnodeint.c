22ed4ef4bcaa011ae637cc5dd7a5be52935da267
Muthoniphoebe <99501695+Muthoniphoebe@users.noreply.github.com>
2022-10-03 13:03:27 +0300

    updated code

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -7,8 +7,8 @@
  *
  * Return: address of the node, or if it does not exist, NULL
  */
- 
- dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	while (index > 0)
 	{
